02fd3ff82b97fc71797f1e78fb79a510a7ce9598
romzy234 <cyrilogoh@gmail.com>
2023-05-27 02:13:46 +0100

    Server Favicon

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/favicon.ico', (req, res) => res.status(204));
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/profile', profileRouter);
